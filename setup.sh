4cde68c4587d6e9a964f781aeb3b7f283441e61e
TAKADA <ri0051rr@ed.ritsumei.ac.jp>
2019-05-24 12:20:14 +0900

    fix bug

--- setup.sh
+++ setup.sh
@@ -4,36 +4,36 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # Windows
 if [ -e %APPDATA%\Code\User ]; then
-    STR = %APPDATA%\Code\User
+    STR=%APPDATA%\Code\User
     cd %APPDATA%\Code\User
 fi
 
 # Mac
 if [ -e $HOME/Library/Application\ Support/Code/User ]; then
-    STR = $HOME/Library/Application\ Support/Code/User
+    STR=$HOME/Library/Application\ Support/Code/User
     cd $HOME/Library/Application\ Support/Code/User
 fi
 # Linux
 if [ -e $HOME/.config/Code/User ]; then
-    STR = $HOME/.config/Code/User
+    STR=$HOME/.config/Code/User
     cd $HOME/.config/Code/User
 fi
 
 
-if [ -e $STR ]; then
-    if [ -e $STR/settings.json ]; then
-    mv settings.json settings.json.bak
+if [ -e "$STR" ]; then
+    if [ -e "$STR"/settings.json ]; then
+    mv -f settings.json settings.json.bak
     fi
-    if [ -e $STR/keybindings.json ]; then
-    mv keybindings.json keybindings.json.bak
+    if [ -e "$STR"/keybindings.json ]; then
+    mv -f keybindings.json keybindings.json.bak
     fi
-    if [ -e $STR/snippets ]; then
-    mv snippets snippets.bak
+    if [ -e "$STR"/snippets ]; then
+    mv -f snippets snippets.bak
     fi
 
-    cp $SCRIPT_DIR/settings.json $STR/settings.json
-    cp $SCRIPT_DIR/keybindings.json $STR/keybindings.json
-    cp $SCRIPT_DIR/snippets $STR/snippets
+    cp $SCRIPT_DIR/settings.json "$STR"/
+    cp $SCRIPT_DIR/keybindings.json "$STR"/
+    cp -r $SCRIPT_DIR/snippets "$STR"/
 
     for extension in `cat $SCRIPT_DIR/extensions.txt`; do
         code --install-extension $extension
