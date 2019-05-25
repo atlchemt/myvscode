1922265d25ff90d3583d1f1928bbb1d80401d035
TAKADA <ri0051rr@ed.ritsumei.ac.jp>
2019-05-26 01:10:49 +0900

    fix bug

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # Windows (cgwin)
-if [ -e $HOME/AppData/Roaming/Code/User ]; then
+if [ -e "$HOME/AppData/Roaming/Code/User" ]; then
     STR=$HOME/AppData/Roaming/Code/User
-    cd $HOME/AppData/Roaming/Code/User
+    cd "$HOME"/AppData/Roaming/Code/User
 fi
 
 # Mac
-if [ -e $HOME/Library/Application\ Support/Code/User ]; then
+if [ -e "$HOME/Library/Application\ Support/Code/User" ]; then
     STR=$HOME/Library/Application\ Support/Code/User
     cd $HOME/Library/Application\ Support/Code/User
 fi
 # Linux
-if [ -e $HOME/.config/Code/User ]; then
+if [ -e "$HOME/.config/Code/User" ]; then
     STR=$HOME/.config/Code/User
     cd $HOME/.config/Code/User
 fi
@@ -31,11 +31,11 @@ if [ -e "$STR" ]; then
     mv -n "$STR"/snippets "$STR"/snippets.bak
     fi
 
-    cp $SCRIPT_DIR/settings.json "$STR"/
-    cp $SCRIPT_DIR/keybindings.json "$STR"/
-    cp -r $SCRIPT_DIR/snippets "$STR"/
+    cp "$SCRIPT_DIR"/settings.json "$STR"/
+    cp "$SCRIPT_DIR"/keybindings.json "$STR"/
+    cp -r "$SCRIPT_DIR"/snippets "$STR"/
 
-    for extension in `cat $SCRIPT_DIR/extensions.txt`; do
+    for extension in `cat "$SCRIPT_DIR"/extensions.txt`; do
         code --install-extension $extension
     done
 fi
